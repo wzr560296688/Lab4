40a008865f0d87dc57133162045237035ae6a1a2
wzr560296688 <398359810@qq.com>
2015-10-23 14:40:29 +0800

    My blog index view created.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+	def show
+	end
+	def new
+	end
+	def create
+	end
+	def edit
+	end
+	def update
+	end
+	def destory
+	end
+end
