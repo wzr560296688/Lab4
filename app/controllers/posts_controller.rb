07d830d1db53064864e691a944c595b3306eec99
wzr560296688 <398359810@qq.com>
2015-10-23 19:49:02 +0800

    My blog edit view created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
@@ -12,14 +13,16 @@ class PostsController < ApplicationController
 		#@post = Post.new(params[:post]) <- older version
 		@post = Post.new(params.require(:post).permit(:title, :content))
 		if @post.save
-		redirect_to posts_path, :notice => “Successfully created!”
+		redirect_to posts_path, :notice => "Successfully created!"
 		else
 		render “new”
 		end
 	end
 	def edit
+		@post = Post.find(params[:id])
 	end
 	def update
+		
 	end
 	def destory
 	end
